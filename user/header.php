e254f7af83d569052a1a85d888651f33dd755f5e
PhongVuYCa <ahongan343@gmail.com>
2022-12-24 02:33:18 +0700

    (#2): add file header

---
+++ user/header.php
@@ -0,0 +1,34 @@
+<header class="row">
+        <div class="logo">
+            &nbsp;&nbsp;
+            <a href="index.php?"><img src="img/logo.png" alt="" width="80px" height="80px"></a>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a class="active" href="index.php">Trang Chủ</a></li>
+                <li><a href="shop.php">Shop</a></li>
+                <li><a href="dmsp.php">Danh Mục Sản Phẩm</a></li>
+                <li><a href="blog.php">Giới Thiệu</a></li>
+                <li><a href="lienhe.php">Liên Hệ</a></li>
+            </ul>
+        </div>
+        
+        <div class ="right-header">
+            <ul class=list_CN>
+                 <li>
+                    <form class="search" action="search.php" method="post">
+                        <li><input type="text" name="tk1" placeholder="Tìm kiếm thông tin" ></li> &emsp;
+                        <li><input type="submit" name="submit" value="Tìm kiếm"></li> &emsp;
+                    </form>
+                </li>
+                <li><a href="basket.php"><img src="img/giohang.png" width="45px" height="35px"></a></li>
+                 <li><a href=""><img src="img/users.png" width="40px" height="40px"></a>
+                    <ul class="sub-login">
+                        <li><a href="registration.php">Đăng ký</a></li>
+                        <li><a href="login.php">Đăng nhập</a></li>
+                    </ul>
+                </li>
+                <li></li>
+            </ul>    
+         </div>
+    </header>
