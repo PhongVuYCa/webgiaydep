56acac73ed870e522769e77486d6b1fdbadba181
PhongVuYCa <ahongan343@gmail.com>
2022-12-24 03:06:35 +0700

    (#11): update file index.php

---
+++ user/index.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>web</title>
+<link rel="stylesheet" href="style.css"/> 
+</head>
+<body >
+<?php 
+    include("header.php");
+?>
+<main>
+    <img src="img/background.jpg" alt="" width="100%" height="auto">
+</main>
+<?php
+    include("footer.php");
+?>
