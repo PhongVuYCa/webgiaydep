81ac78c1f4c9f30cb8359330352f6365676caef9
PhongVuYCa <ahongan343@gmail.com>
2022-12-24 21:23:33 +0700

    (#2): add file header.php

---
+++ admin/header.php
@@ -0,0 +1,22 @@
+<header class="row">
+        <div class="logo">
+            &nbsp;&nbsp;
+            <a href="index.php?"><img src="img/logo.png" alt="" width="80px" height="80px"></a>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="index.php?">Trang Chủ</a></li>
+                <li><a href="">Danh sách khách hàng</a></li>
+                <li><a href="">Danh sách Sản Phẩm</a></li>
+                <li><a href="">Đơn hàng</a></li>
+            </ul>
+        </div>
+        
+        <div class ="right-header">
+            <ul class=list_CN>
+                 <li><a href="login.php"><img src="img/users.png" width="40px" height="40px"></a>
+                
+                </li>
+            </ul>    
+         </div>
+    </header>
