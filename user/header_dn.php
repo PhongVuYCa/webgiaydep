69ad69b293a4e5b232bb60c90328abe71a4b3f72
PhongVuYCa <ahongan343@gmail.com>
2022-12-24 16:06:59 +0700

    (#15): update file header_dn.php

--- user/header_dn.php
+++ user/header_dn.php
@@ -5,27 +5,27 @@
 <header class="row">
         <div class="logo">
             &nbsp;&nbsp;
-            <a href="index.php?"><img src="img/logo.png" alt="" width="80px" height="80px"></a>
+            <a href="home.php?"><img src="img/logo.png" alt="" width="80px" height="80px"></a>
         </div>
         <div class="menu">
             <ul>
-                <li><a class="active" href="index.php">Trang Chủ</a></li>
-                <li><a href="shop.php">Shop</a></li>
-                <li><a href="dmsp.php">Danh Mục Sản Phẩm</a></li>
-                <li><a href="blog.php">Giới Thiệu</a></li>
-                <li><a href="lienhe.php">Liên Hệ</a></li>
+                <li><a class="active" href="home.php">Trang Chủ</a></li>
+                <li><a href="shop1.php">Shop</a></li>
+                <li><a href="dmsp1.php">Danh Mục Sản Phẩm</a></li>
+                <li><a href="blog1.php">Giới Thiệu</a></li>
+                <li><a href="lienhe1.php">Liên Hệ</a></li>
             </ul>
         </div>
         
         <div class ="right-header">
             <ul class=list_CN>
                  <li>
-                    <form class="search" action="search.php" method="post">
+                    <form class="search" action="search1.php" method="post">
                         <li><input type="text" name="tk1" placeholder="Tìm kiếm thông tin" ></li> &emsp;
                         <li><input type="submit" name="submit" value="Tìm kiếm"></li> &emsp;
                     </form>
                 </li>
-                <li><a href="basket.php"><img src="img/giohang.png" width="45px" height="35px"></a></li>
+                <li><a href="basket1.php"><img src="img/giohang.png" width="45px" height="35px"></a></li>
                 <div class="menuuu">
                  <li><a href="information.php"><img src="img/users.png" width="40px" height="40px"></a>
                  <ul class="menuuu1">
