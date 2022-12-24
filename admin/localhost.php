ccc493e60b4b0d9feacc91dc0afd043978ca636c
PhongVuYCa <ahongan343@gmail.com>
2022-12-24 22:23:40 +0700

    (#3): add file ket noi co so du lieu

---
+++ admin/localhost.php
@@ -0,0 +1,13 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "data";
+
+    // tạo kết nối
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // kiểm kết nối
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+?>
